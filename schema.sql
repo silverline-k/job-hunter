49698280aee8dcbcf29ada42f992b42e8dac8018
silverline-k <riv2rsilv2rlin2@gmail.com>
2023-12-16 01:53:30 +0900

    mysql 연동

---
+++ schema.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE job_hunter;
+
+CREATE TABLE job_hunter.listing (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    position_index INT,
+    position_name VARCHAR(255),
+    position_title VARCHAR(255),
+    company_name VARCHAR(255),
+    company_location VARCHAR(255),
+    url VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP DEFAULT NULL
+);
